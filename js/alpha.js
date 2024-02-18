cd042198fbe1d3a4d614219a35d312dfb2786fd8
Zahidcst15 <zahid.cst15@gmail.com>
2024-02-19 00:19:36 +0600

    set game over score and restart game and quit the game

--- js/alpha.js
+++ js/alpha.js
@@ -7,10 +7,26 @@ function play(){
     const playGroundSection = document.getElementById('playground');
     playGroundSection.classList.remove('hidden');
     continueGame();
+
+    // score screen
+    const scoreGround = document.getElementById('final-score');
+    scoreGround.classList.add('hidden'); 
+
+    // reset score and life
+    setTextElementValueById('initial-life', 5);
+    setTextElementValueById('current-score', 0);
+
+    
 }
 
 function handelKeyboardButtonPress(event){
     const playerPressed = event.key;
+
+    // quit the game if pressed 'Esc'
+    if(playerPressed === 'Escape'){
+        gameOver();
+    }
+
 
     // get the expected to press
     const currentAlphabetElement = document.getElementById('current-alphabet');
@@ -34,6 +50,10 @@ function handelKeyboardButtonPress(event){
         const currentLife = getTextElementValueById('initial-life');
         const updatedLife = currentLife - 1;
         setTextElementValueById('initial-life', updatedLife);
+
+        if(updatedLife === 0){
+           gameOver();
+        }
     }
 }
 
@@ -91,4 +111,28 @@ function getTextElementValueById(elementId){
 function setTextElementValueById(elementId, value){
     const element = document.getElementById(elementId);
     element.innerText = value;
+}
+
+
+function gameOver(){
+    const playGroundSection = document.getElementById('playground');
+    playGroundSection.classList.add('hidden'); 
+
+    const scoreGround = document.getElementById('final-score');
+    scoreGround.classList.remove('hidden'); 
+
+    // update final score
+    const lastScore = getTextElementValueById('current-score');
+    setTextElementValueById('game-score', lastScore);
+
+    // clear the last selected alphabet bg color
+    const alphabets = getTextElementTextId('current-alphabet');
+    removeBackgroundColorById(alphabets);
+}
+
+
+function getTextElementTextId(elementId){
+   const element = document.getElementById(elementId);
+   const text = element.innerText;
+   return text;
 }
